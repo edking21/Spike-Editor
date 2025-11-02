c9475843d2307ae915a26be1be4048c2efdb65a6
Ed King <edking21@gmail.com>
2025-11-02 07:48:41 -0600

    Refactor line counter implementation and add hand wave detection functionality

--- Python/hand_wave.py
+++ Python/hand_wave.py
@@ -1,4 +1,4 @@
-# Line Counter Python-1
+# Import statements - these bring in special functions we need for our robot
 import runloop, time, sys, motor_pair, motor, force_sensor, runloop # pyright: ignore[reportMissingImports]
 import color, color_sensor, distance_sensor # pyright: ignore[reportMissingImports]
 from hub import light, light_matrix, port, sound # pyright: ignore[reportMissingImports]
@@ -6,6 +6,7 @@ from time import sleep, sleep_ms
 from runloop import run # pyright: ignore[reportMissingImports]
 
 # Variables to count how many times we see each color
+# Think of these like scoreboards that keep track of points
 red_count = 0
 yellow_count = 0
 blue_count = 0
@@ -16,6 +17,7 @@ yellow = 7
 red = 9
 
 # Connect two motors together so they work as a team
+# Port C and D are where we plugged in our motors
 motor_pair.pair(motor_pair.PAIR_1, port.C, port.D)
 
 # Remember the last color we saw so we don't count the same color twice in a row
@@ -29,16 +31,22 @@ should_stop = False
 async def check_blue():
     # Use "global" to access variables from outside this function
     global blue_count, last_color, should_stop
-
+    
+    # Keep checking until we're told to stop
     while not should_stop:
         # Ask the color sensor what color it sees right now
         current_color = color_sensor.color(port.F)
-
+        
+        # If we see blue AND it's different from the last color we saw
         if current_color == blue and last_color != blue:
+            # Light up the hub with the color we found
             light.color(light.POWER, current_color)
+            # Remember this color for next time
             last_color = blue
+            # Add 1 to our blue counter
             blue_count += 1
-
+        
+        # Wait a tiny bit before checking again (100 milliseconds)
         await runloop.sleep_ms(100)
 
 # This function watches for yellow colors (works just like check_blue)
@@ -68,15 +76,17 @@ async def check_red():
 # This function watches for someone waving their hand near the robot
 async def check_hand_wave():
     global blue_count, yellow_count, should_stop
-
+    
     while not should_stop:
         # Check how far away the nearest object is (in millimeters)
         distance = distance_sensor.distance(port.B)
+        print("Distance {:6.2f}".format(distance))
 
-        # If something is close (less than 50mm away) and the sensor is working
+        # If something is very close (less than 50mm away) and the sensor is working
         if distance != -1 and distance < 50:
+            # Stop the motors from moving
             motor_pair.stop(motor_pair.PAIR_1)
-            # Tell all other functions to stop
+            # Tell all other functions to stop too
             should_stop = True
             # Show our final results - how many of each color we counted
             print("Blue count:{:2d}  Yellow count:{:2d}  Red count:{:2d}".format(blue_count, yellow_count, red_count))
@@ -86,6 +96,7 @@ async def check_hand_wave():
         # Wait a bit before checking distance again
         await runloop.sleep_ms(100)
 
+# This is our main function - it starts everything up
 async def main():
     # Start the motors moving forward at normal speed
     motor_pair.move(motor_pair.PAIR_1, 0)
@@ -94,5 +105,7 @@ async def main():
     # It's like having multiple people doing different jobs simultaneously
     run(check_blue(), check_yellow(), check_red(), check_hand_wave())
 
+# Start our main function and run the whole program
 runloop.run(main())
+# When everything is done, exit the program
 sys.exit()
