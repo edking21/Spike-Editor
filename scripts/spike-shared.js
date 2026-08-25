2920ea9f171bbfb10d2c24543467afbbf4518bd7
Ed King <edking21@gmail.com>
2026-08-25 09:22:19 -0500

    Update getting started section indices for consistency

--- scripts/spike-shared.js
+++ scripts/spike-shared.js
@@ -977,7 +977,7 @@ await motor_pair.move_for_degrees(motor_pair.PAIR_1, 10 * 360, 180)`
                 }
             ]
         },
-        12: {   // Getting Started 
+        20: {   // Getting Started 
             colorClass: 'gettingstarted-color',
             snippets: [
                 {
